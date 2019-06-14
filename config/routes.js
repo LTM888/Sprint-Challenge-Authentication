4d6e47a1fad9ccde1059ba8d54512042d5338b6b
[your <leigh.tm@gmail.com>
2019-06-14 14:09:58 -0400

    almost at MVP endpoits work

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,10 @@
-const axios = require('axios');
+const axios = require('axios');;
+const bcrypt = require('bcryptjs');
 
-const { authenticate } = require('../auth/authenticate');
+
+const Users = require("../config/users-model.js");
+
+const { authenticate, generateToken } = require('../auth/authenticate');
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -10,11 +14,46 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 9);
+  user.password = hash;
+
+  Users.add(user)
+
+    .then(user => {
+      console.log("add user:", user);
+      const token = generateToken(user);
+      console.log("registration token:", token);
+      res.status(201).json({ user, token });
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
 
 function login(req, res) {
   // implement user login
+  let { username, password } = req.body;
+  Users.findBy({ username })
+
+    .first()
+    .then(user => {
+      console.log(user);
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token
+        });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
+
 
 function getJokes(req, res) {
   const requestOptions = {
